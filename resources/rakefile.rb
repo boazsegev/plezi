55b39ec841a423f8164f4aa83175ea2d9fa001ec
Myst <We try, we fail, we do, we are>
2014-10-10 22:51:56 +0300

    gem file preservation on rebuild

--- resources/rakefile.rb
+++ resources/rakefile.rb
@@ -2,21 +2,18 @@
 BUILDING_ANOREXIC_TEMPLATE = true
 NO_ANOREXIC_AUTO_START = true
 
+require 'rubygems'
+require 'rake'
+
 app_name = ::File.expand_path(::Dir["."][0]).split(/[\\\/]/).last
 require ::File.expand_path(::Dir["."][0], ( app_name + ".rb") )
 
 namespace :app do
 
-	desc "adds and framework files that might be missing (use after adding anorexic gems)."
+	desc "adds and framework files that might be missing (use after adding anorexic gems).\nnotice: this will not update rakefile.rb or Gemfile(!)."
 	task :rebuild do
-		# require gem and add the app to the tree
-		puts "coming soon..."
-		old = ARGV[1]
-		ARGV[1] = app_name
-		template = AppTemplate.new(app_name)
 		Dir.chdir '..'
-		template.build app_name
-		ARGV[1] = old
+		puts `anorexic force #{app_name}`
 	end
 end
 
