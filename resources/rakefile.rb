699e5634464fdc784ed8ee52bd822cf513799045
Myst <We try, we fail, we do, we are>
2014-10-10 22:31:20 +0300

    working on rake

--- resources/rakefile.rb
+++ resources/rakefile.rb
@@ -1,5 +1,6 @@
 # encoding: UTF-8
-BUILDING_ANOREXIC_TEMPLATE = NO_ANOREXIC_AUTO_START = true
+BUILDING_ANOREXIC_TEMPLATE = true
+NO_ANOREXIC_AUTO_START = true
 
 app_name = ::File.expand_path(::Dir["."][0]).split(/[\\\/]/).last
 require ::File.expand_path(::Dir["."][0], ( app_name + ".rb") )
@@ -10,9 +11,12 @@ namespace :app do
 	task :rebuild do
 		# require gem and add the app to the tree
 		puts "coming soon..."
-		template = AppTemplate.new 
+		old = ARGV[1]
+		ARGV[1] = app_name
+		template = AppTemplate.new(app_name)
 		Dir.chdir '..'
 		template.build app_name
+		ARGV[1] = old
 	end
 end
 
