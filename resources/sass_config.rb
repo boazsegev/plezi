1186ba78b0a75cc092bcc1c845333b95ed91a9ae
Myst <We try, we fail, we do, we are>
2014-10-12 05:47:11 +0300

    sass with rack support

--- resources/sass_config.rb
+++ resources/sass_config.rb
@@ -7,12 +7,17 @@ if defined? Sass
 
 	# sets the render style to be compact.
 	Sass::Plugin.options[:style] = :compact
+
 	# sets the cache location for caching (caches by default)
 	Sass::Plugin.options[:cache_location] = Root.join('tmp', 'sass-cache').to_s
 	# sets the location for the saas root folder.
-	Sass::Plugin.options[:template_location] = Root.join('assets').to_s
+	Sass::Plugin.options[:template_location] = Root.join('assets', 'sass').to_s
 	# sets the location for rendered files (for static serving).
 	Sass::Plugin.options[:css_location] = Root.join('public', 'assets').to_s
+
+	# on Heroku it is not possible to write the css files to the public folder.
+	# in reality, it is better to disable Sass for production and sync rendered files.
+	Anorexic.default_middleware.pop if ENV['DYNO'] || ENV['RACK_ENV'] == 'production'
 end
 
 if defined? Rack::Coffee
