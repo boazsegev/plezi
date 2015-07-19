613a7f9b9990fe59c4e1fe6f282bcaa3e8e43ee2
Boaz Segev <boaz@2be.co.il>
2015-07-19 02:35:49 -0400

    Remove backup data

--- lib/plezi 0.9/oauth.rb
+++
@@ -1,5 +0,0 @@
-# requires the open-uri library.
-# require 'open-uri'
-require 'net/http'
-# require the OAuth2 controller
-require 'plezi/oauth/auth_controller.rb'
--- lib/plezi 0.9/version.rb
+++
@@ -1,3 +0,0 @@
-module Plezi
-    VERSION = "0.9.1"
-end
