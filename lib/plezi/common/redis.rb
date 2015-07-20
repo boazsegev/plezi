8810f8216f38ea0554923623bfc1ad3da8b6774e
Boaz Segev <boaz@2be.co.il>
2015-07-20 10:35:15 -0400

    protect reds from multi-threading

--- lib/plezi/common/redis.rb
+++ lib/plezi/common/redis.rb
@@ -13,29 +13,33 @@ module Plezi
 	def redis_connection
 		return @redis if (@redis_sub_thread && @redis_sub_thread.alive?) && @redis
 		return false unless defined?(Redis) && ENV['PL_REDIS_URL']
-		@redis_uri ||= URI.parse(ENV['PL_REDIS_URL'])
-		@redis ||= Redis.new(host: @redis_uri.host, port: @redis_uri.port, password: @redis_uri.password)
-		raise "Redis connction failed for: #{ENV['PL_REDIS_URL']}" unless @redis
-		@redis_sub_thread = Thread.new do
-			begin
-			Redis.new(host: @redis_uri.host, port: @redis_uri.port, password: @redis_uri.password).subscribe(Plezi::Settings.redis_channel_name) do |on|
-					on.message do |channel, msg|
-						begin
-							data = YAML.load(msg)
-							next if data[:server] == Plezi::Settings.uuid
-							if data[:target]
-								GRHttp::Base::WSHandler.unicast data[:target], data
-							else
-								GRHttp::Base::WSHandler.broadcast data
+		@redis_locker ||= Mutex.new
+		@redis_locker.synchronize do
+			return @redis if (@redis_sub_thread && @redis_sub_thread.alive?) && @redis # repeat the test once syncing is done.
+			@redis_uri ||= URI.parse(ENV['PL_REDIS_URL'])
+			@redis ||= Redis.new(host: @redis_uri.host, port: @redis_uri.port, password: @redis_uri.password)
+			raise "Redis connction failed for: #{ENV['PL_REDIS_URL']}" unless @redis
+			@redis_sub_thread = Thread.new do
+				begin
+				Redis.new(host: @redis_uri.host, port: @redis_uri.port, password: @redis_uri.password).subscribe(Plezi::Settings.redis_channel_name) do |on|
+						on.message do |channel, msg|
+							begin
+								data = YAML.load(msg)
+								next if data[:server] == Plezi::Settings.uuid
+								if data[:target]
+									GRHttp::Base::WSHandler.unicast data[:target], data
+								else
+									GRHttp::Base::WSHandler.broadcast data
+								end
+							rescue => e
+								Reactor.error e
 							end
-						rescue => e
-							Reactor.error e
 						end
 					end
+				rescue => e
+					Reactor.error e
+					retry
 				end
-			rescue => e
-				Reactor.error e
-				retry
 			end
 		end
 		@redis
