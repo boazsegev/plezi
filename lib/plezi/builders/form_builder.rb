aa15e08fe4efe622746e92d501c2e4702605a496
Boaz Segev <boaz@2be.co.il>
2015-07-28 23:58:59 -0400

    future thoughts

--- lib/plezi/builders/form_builder.rb
+++ lib/plezi/builders/form_builder.rb
@@ -1,4 +1,24 @@
+require 'plezi/builders/builder'
+
 module Plezi
 
+	module FormBuilder
 
+		FORM_BUILDERS = %w{ Slim ERB }
+		MODEL_BUILDERS = %w{ Squel }
+
+		DB_TYPES = %w{primary_key string text integer float decimal datetime time date binary boolean}
+
+		@gem_root = ::Plezi::Builder::GEM_ROOT
+		def self.parse_args
+			return unless ARGS[0][0] == 'g'
+			struct = {}
+			ARGS[1..-1].each do |s|
+				s = s.split /[\:\.]/
+				raise "Cannot parse parameters - need to be defined as name.type or name:type." if s.count !=2
+				struct[s[0]] = DB_TYPES[s[1].downcase] || (raise "Unrecognized type #{s[1]}.")
+			end
+			struct
+		end
+	end
 end
