962457f469aa94a5e0cc95439a62cd6669c241a7
Boaz Segev <Boaz@2be.co.il>
2015-11-15 04:23:42 -0500

    rake support

--- lib/plezi/rake.rb
+++ lib/plezi/rake.rb
@@ -2,15 +2,18 @@
 Iodine.protocol = false
 
 
-namespace :make
+namespace :make do
 	# add ActiveRecord controller-model generator
 
 	# add Squel controller-model generator
 
 end
 # add console mode
-desc "Start the application as a console with no server."
-task :irb do
-  require 'irb'
-  IRB.start
+desc "Same as `plezi c`: starts the application as a console, NOT a server."
+task :console do
+	Kernel.exec "plezi c"
+end
+desc "Same as `rake console`: starts the application as a console, NOT a server."
+task :c do
+	Kernel.exec "plezi c"
 end
