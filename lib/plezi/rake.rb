02ab238934892216d758158943390853ad442f3f
Boaz Segev <Boaz@2be.co.il>
2015-11-30 03:15:53 -0500

    make sure rake support has rake

--- lib/plezi/rake.rb
+++ lib/plezi/rake.rb
@@ -1,19 +1,24 @@
-# Make sure the server doesn't start
-Iodine.protocol = false
+# make sure this file only runs within the context of a Rake enabled script
+if defined? Rake
+	
+	# Make sure the server doesn't start
+	Iodine.protocol = false
 
 
-namespace :make do
-	# add ActiveRecord controller-model generator
+	namespace :make do
+		# TODO: add ActiveRecord controller-model generator
 
-	# add Squel controller-model generator
+		# TODO: add Squel controller-model generator
 
-end
-# add console mode
-desc "Same as `plezi c`: starts the application as a console, NOT a server."
-task :console do
-	Kernel.exec "plezi c"
-end
-desc "Same as `rake console`: starts the application as a console, NOT a server."
-task :c do
-	Kernel.exec "plezi c"
+	end
+	# add console mode
+	desc "Same as `plezi c`: starts the application as a console, NOT a server."
+	task :console do
+		Kernel.exec "plezi c"
+	end
+	desc "Same as `rake console`: starts the application as a console, NOT a server."
+	task :c do
+		Kernel.exec "plezi c"
+	end
+
 end
