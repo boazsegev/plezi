3f4ca8b105aa50bc0d35e8c04e6dfba96a5bf541
Boaz Segev <Boaz@2be.co.il>
2016-08-02 00:08:43 -0400

    tested (and fixed) routing

--- lib/plezi/controller/controller.rb
+++ lib/plezi/controller/controller.rb
@@ -3,6 +3,7 @@ module Plezi
     module Controller
       def self.included(base)
         base.extend ::Plezi::Base::Controller::ClassMethods
+        base._pl_init_class_data
       end
 
       attr_reader :request, :response, :params
@@ -34,40 +35,52 @@ module Plezi
           REST_METHODS.each { |m| mths.delete m }
           mths.each { |m| @_pl_get_map[m.to_s.freeze] = m }
 
-          _pl_has_delete
-          _pl_has_update
-          _pl_has_create
           @_pl_get_map
         end
 
         def _pl_has_delete
-          return @_pl_has_delete unless @_pl_has_delete.nil?
-          @_pl_has_delete = public_instance_methods.include?(:delete)
+          @_pl_has_delete
         end
 
         def _pl_has_update
-          return @_pl_has_update unless @_pl_has_update.nil?
-          @_pl_has_update = public_instance_methods.include?(:update)
+          @_pl_has_update
         end
 
         def _pl_has_create
-          return @_pl_has_create unless @_pl_has_create.nil?
-          @_pl_has_create = public_instance_methods.include?(:create)
+          @_pl_has_create
+        end
+
+        def _pl_has_show
+          @_pl_has_show
         end
 
         def _pl_params2method(params)
           # puts "matching against #{params}"
           case params['_method'.freeze]
           when :get # since this is common, it's pushed upwards.
-            _pl_get_map[params['id'.freeze]]
+            return (_pl_get_map[params['id'.freeze]] || (_pl_has_show && :show) || nil)
           when :put, :patch
+            return :create if _pl_has_create && (params['id'.freeze].nil? || params['id'.freeze] == 'new')
             return :update if _pl_has_update
           when :post
             return :create if _pl_has_create
           when :delete
             return :delete if _pl_has_delete
           end
-          _pl_get_map[params['id'.freeze]]
+          (_pl_get_map[params['id'.freeze]] || (_pl_has_show && :show) || nil)
+        end
+
+        def _pl_init_class_data
+          @_pl_get_map = nil
+          @_pl_has_show = public_instance_methods.include?(:show)
+          @_pl_has_create = public_instance_methods.include?(:create)
+          @_pl_has_update = public_instance_methods.include?(:update)
+          @_pl_has_delete = public_instance_methods.include?(:delete)
+          _pl_get_map
+          _pl_has_delete
+          _pl_has_update
+          _pl_has_create
+          _pl_has_show
         end
       end
     end
--- test/route_test.rb
+++ test/route_test.rb
@@ -1,8 +1,33 @@
 require 'test_helper'
+require 'stringio'
 
 class Stub
   def index
-    'Hello test'
+    self.class.last_call = requested_method
+  end
+
+  def show
+    self.class.last_call = requested_method
+  end
+
+  def new
+    self.class.last_call = requested_method
+  end
+
+  def create
+    self.class.last_call = requested_method
+  end
+
+  def update
+    self.class.last_call = requested_method
+  end
+
+  def delete
+    self.class.last_call = requested_method
+  end
+
+  class << self
+    attr_accessor :last_call
   end
 end
 
@@ -25,5 +50,51 @@ class RouteTest < Minitest::Test
     assert route.match('/here/Boston/hand/1')
     params = Thread.current["Route#{route.object_id.to_s(16)}".to_sym]
     assert (params == { 'id' => '1', 'go' => { 'home' => 'Boston', 'left' => 'hand' } }), params.to_s
+  end
+
+  def test_it_maps_to_rest
+    Plezi.no_autostart
+    response = Rack::Response.new
+
+    env = { 'PATH_INFO' => '/resource', 'REQUEST_METHOD' => 'GET', 'QUERY' => '', 'rack.input' => StringIO.new('') }
+    route = Plezi.route('/resource', Stub).last
+    request = Rack::Request.new(env)
+    route.call(request, response)
+    assert (Stub.last_call == :index), "REST :index wasn't mapped correctly #{Stub.last_call}"
+
+    env = { 'PATH_INFO' => '/resource/1', 'REQUEST_METHOD' => 'GET', 'QUERY' => '', 'rack.input' => StringIO.new('') }
+    request = Rack::Request.new(env)
+    route.call(request, response)
+    assert (Stub.last_call == :show), "REST :show wasn't mapped correctly #{Stub.last_call}"
+
+    env = { 'PATH_INFO' => '/resource/new', 'REQUEST_METHOD' => 'GET', 'QUERY' => '', 'rack.input' => StringIO.new('') }
+    request = Rack::Request.new(env)
+    route.call(request, response)
+    assert (Stub.last_call == :new), "REST :new wasn't mapped correctly #{Stub.last_call}"
+
+    env = { 'PATH_INFO' => '/resource', 'REQUEST_METHOD' => 'POST', 'QUERY' => '', 'rack.input' => StringIO.new('') }
+    request = Rack::Request.new(env)
+    route.call(request, response)
+    assert Stub.last_call == :create, "REST :create wasn't mapped correctly #{Stub.last_call}"
+
+    env = { 'PATH_INFO' => '/resource', 'REQUEST_METHOD' => 'PUT', 'QUERY' => '', 'rack.input' => StringIO.new('') }
+    request = Rack::Request.new(env)
+    route.call(request, response)
+    assert Stub.last_call == :create, "REST :create wasn't mapped correctly #{Stub.last_call}"
+
+    env = { 'PATH_INFO' => '/resource/1', 'REQUEST_METHOD' => 'PUT', 'QUERY' => '', 'rack.input' => StringIO.new('') }
+    request = Rack::Request.new(env)
+    route.call(request, response)
+    assert Stub.last_call == :update, "REST :update wasn't mapped correctly #{Stub.last_call}"
+
+    env = { 'PATH_INFO' => '/resource/1', 'REQUEST_METHOD' => 'PATCH', 'QUERY' => '', 'rack.input' => StringIO.new('') }
+    request = Rack::Request.new(env)
+    route.call(request, response)
+    assert Stub.last_call == :update, "REST :update wasn't mapped correctly #{Stub.last_call}"
+
+    env = { 'PATH_INFO' => '/resource/1', 'REQUEST_METHOD' => 'DELETE', 'QUERY' => '', 'rack.input' => StringIO.new('') }
+    request = Rack::Request.new(env)
+    route.call(request, response)
+    assert Stub.last_call == :delete, "REST :delete wasn't mapped correctly #{Stub.last_call}"
   end
 end
