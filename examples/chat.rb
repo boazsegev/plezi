b7ec5e5fb81609f010cf6a0e32907596431b2bf2
Bo <Boaz@2be.co.il>
2017-06-20 22:13:21 -0400

    Fix example and show off the Redis engine extension

--- examples/chat.rb
+++ examples/chat.rb
@@ -2,7 +2,7 @@
 require 'plezi'
 class ChatServer
   def index
-    render 'client'
+    "Use Websockets to connect."
   end
   def on_open
     return close unless params['id']
@@ -10,9 +10,24 @@ class ChatServer
     subscribe channel: "chat"
     publish channel: "chat", message: "#{@name} joind the chat."
     write "Welcome, #{@name}!"
+    # if we have Redis
+    if(Iodine.default_pubsub.is_a? Iodine::PubSub::RedisEngine)
+      # We'll add the name to the list of people in the chat.
+      # Blocks are used as event callbacks and are executed asynchronously.
+      Iodine.default_pubsub.send("SADD", "chat_members", @name) do
+        # after the name was added, we'll get all the current people in the chat
+        Iodine.default_pubsub.send("SMEMBERS", "chat_members") do |members|
+          # By now, we're outside the Websocket connection's lock.
+          # To safely access the connection, we'll use `defer`
+          defer { write "Currently in the chatroom: #{members.join ', '}" }
+        end
+      end
+    end
   end
   def on_close
     publish channel: "chat", message: "#{@name} joind the chat."
+    # if we have Redis
+    Iodine.default_pubsub.send("SREM", "chat_members", @name) if(Iodine.default_pubsub.is_a? Iodine::PubSub::RedisEngine)
   end
   def on_message data
     publish channel: "chat", message: "#{@name}: #{data}"
